aeb7acab9110af4295ce16aef6fd21533bfa1ca6
Angel Uriot <angel.uriot@gmail.com>
2023-05-22 01:43:46 +0200

    Safari final fix

--- js/script.js
+++ js/script.js
@@ -7,11 +7,36 @@ async function in_animations()
 	about_in_animations();
 }
 
+function fix_safari()
+{
+	let all = document.querySelectorAll('*');
+
+	for (let element of all)
+	{
+		element.style.filter = 'none';
+		element.style.webkitFilter = 'none';
+	}
+
+	let all_transitions = [
+		document.querySelectorAll('#experience_section .job_view a'),
+		document.querySelectorAll('#experience_section .job_text .text'),
+		document.querySelectorAll('#experience_section .job'),
+		document.querySelectorAll('#projects_section .project_view a'),
+		document.querySelectorAll('#projects_section .project_text .text'),
+		document.querySelectorAll('#projects_section .project')
+	];
+
+	for (let list of all_transitions)
+		for (let element of list)
+		{
+			console.log('done')
+			element.style.transition = 'none';
+			element.style.webkitTransition = 'none';
+		}
+}
+
 window.onload = () =>
 {
-	if (!is_safari())
-		document.querySelector('#safari_warning').style.display = 'none';
-
 	document.documentElement.scrollLeft = 0;
 
 	header_events();
@@ -22,6 +47,16 @@ window.onload = () =>
 
 	in_animations();
 	videos_events();
+
+	if (!is_safari())
+		document.querySelector('#safari_warning').style.display = 'none';
+	else
+	{
+		fix_safari();
+		window.setTimeout(fix_safari, 1000);
+		window.setTimeout(fix_safari, 2000);
+		window.setTimeout(fix_safari, 5000);
+	}
 
 	document.querySelector('#loading_screen').style.display = 'none';
 };
