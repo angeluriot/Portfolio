ebd20bcacdb49207d31fbd86858b54de612dbc67
Angel Uriot <angel.uriot@gmail.com>
2023-05-26 21:02:01 +0200

    lighthouse updates

--- js/projects.js
+++ js/projects.js
@@ -68,7 +68,7 @@ function projects_events()
 						</div>
 						<div class="project_view">
 							<a ${is_safari() ? 'class="safari_fix"' : ''} href="${project.links[0]}" target="_blank">
-								<img src="${project.image}" width="720px" height="405px"/>
+								<img src="${project.image}" alt="${project.title.toLowerCase()} image" width="720px" height="405px"/>
 								` + (project.video == 'none' || is_safari() ? '' : `
 								<div class="lds-ring"><div></div><div></div><div></div><div></div></div>
 								<video loop muted preload="metadata">
@@ -120,14 +120,14 @@ function projects_events()
 					<div class="other_project_content">
 						<div class="header">
 							<div class="logos">
-								<img src="${project.logo}" width="45px" height="45px"/>
+								<img src="${project.logo}" alt="${project.title.toLowerCase()} logo" width="45px" height="45px"/>
 								<div class="links">
 									` + (project.links[1] == 'none' ? '' : `
-									<a class="github" href="${project.links[1]}" target="_blank">
+									<a class="github" href="${project.links[1]}" aria-label="github" target="_blank">
 										<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 19.05 20.31"><g><path d="M7.26 16.34c-4.11 1.23-4.11-2.06-5.76-2.47M13 18.81V15.62a2.78 2.78 0 0 0-.77-2.15c2.59-.28 5.3-1.26 5.3-5.76a4.46 4.46 0 0 0-1.23-3.08 4.18 4.18 0 0 0-.08-3.11s-1-.29-3.22 1.22a11 11 0 0 0-5.76 0C5 1.23 4 1.52 4 1.52A4.18 4.18 0 0 0 4 4.63 4.48 4.48 0 0 0 2.73 7.74c0 4.46 2.72 5.44 5.31 5.76a2.8 2.8 0 0 0-.78 2.12v3.19"/></g></svg>
 									</a>
 									`) + (project.links[2] == 'none' ? '' : `
-									<a class="test" href="${project.links[2]}" target="_blank">
+									<a class="test" href="${project.links[2]}" aria-label="test" target="_blank">
 										<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 17.09 18.64"><g><path d="M14.55 7.52 4.62 1.78A2.08 2.08 0 0 0 1.5 3.58V15.05a2.08 2.08 0 0 0 3.12 1.8l9.93-5.73A2.08 2.08 0 0 0 14.55 7.52Z"/></g></svg>
 									</a>
 									`) + `
